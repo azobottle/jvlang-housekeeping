8752e8c451c6a6614cb0a905c511c356b9b01b0c
az0bottle <1553359439@qq.com>
2024-03-31 19:56:27 +0800

    feat:shifu app nav bar

---
+++ androidshifu/app/src/main/java/com/jvlang/housekeeping/ui/screen.kt
@@ -0,0 +1,23 @@
+package com.jvlang.housekeeping.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun ActiveOrder(navHostController: NavHostController) {
+    Column {
+        Text("订单")
+
+    }
+}
+
+
+@Composable
+fun Mine(navHostController: NavHostController) {
+    Column {
+        Text("Friends List")
+
+    }
+}
